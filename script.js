4b1e6559f7c412fb77878fd9fa441a638eda5574
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-13 13:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function btnClick() {
 	val = document.getElementById("counter")
-	alert(val.innerHTML)
+	alert(parseInt(val.innerHTML))
 	val.innerHTML = parseInt(val.innerHTML)+1
 }
